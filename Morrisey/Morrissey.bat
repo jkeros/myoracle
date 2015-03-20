e763629ed309ead9382375d22becbc52a0e750a8
Julie Keros <julieker@umich.edu>
2015-03-20 15:14:50 -0400

    initial version

---
+++ Morrisey/Morrissey.bat
@@ -0,0 +1,12 @@
+set _my_datetime=%date%_%time%
+
+
+set _my_datetime=%_my_datetime: =_%
+set _my_datetime=%_my_datetime::=%
+set _my_datetime=%_my_datetime:/=_%
+set _my_datetime=%_my_datetime:.=_%
+
+
+c:\AutoMated_DataLoad_Jobs\test\morrissey\morrissey.vbs  > c:\AutoMated_DataLoad_Jobs\TEST\morrissey\LOG\morrissey_%_my_datetime%.log
+c:\AutoMated_DataLoad_Jobs\test\morrissey\scriptcopystagingprodmorrissey.bat  > c:\AutoMated_DataLoad_Jobs\TEST\morrissey\LOG\scriptcopystagingprodmorrissey_%_my_datetime%.log
+c:\AutoMated_DataLoad_Jobs\test\morrissey\moriseeydir.bat > c:\AutoMated_DataLoad_Jobs\TEST\morrissey\LOG\dir.log
---
+++ Morrisey/control/morrissey - Copy.ctl
@@ -0,0 +1,25 @@
+OPTIONS
+(
+SKIP=1
+)
+LOAD DATA
+INFILE "morrisey.csv"
+BADFILE "morrisey.bad"
+DISCARDFILE "morrisey.dsc"
+TRUNCATE
+INTO TABLE MORRISSEY_RAW
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+TRAILING NULLCOLS
+(
+HBG_Group_ID "TRIM(:HBG_Group_ID)",	
+HBG_nm "TRIM(:HBG_Nm)",
+HBG_Division_cd "TRIM(:HBG_Division_cd)",	
+HBG_Unique_ID "TRIM(:HBG_Unique_ID)",	
+Last_nm "TRIM(:Last_nm)",	
+First_nm "TRIM(:First_nm)",	
+gender_cd "substr(:gender_cd,1,1)",	
+dob_dt date 'MM/DD/YYYY',
+group_size_cd "TRIM(:group_size_cd)",
+relationship_cd "SUBSTR(:relationship_cd,1,1)",
+email_txt "TRIM(:email_txt)"
+)

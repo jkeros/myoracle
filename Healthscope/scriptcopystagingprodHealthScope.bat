274f005a7f9a0d4c90ae862200e1024cad19e870
Julie Keros <julieker@umich.edu>
2015-03-20 15:23:48 -0400

    initial version

--- Healthscope/scriptcopystagingprodHealthScope.bat
+++ Healthscope/scriptcopystagingprodHealthScope.bat
@@ -7,13 +7,16 @@ set _my_datetime=%_my_datetime:/=_%
 set _my_datetime=%_my_datetime:.=_%
 
 
-sqlplus julieker/hmrc$$julie1015_2@hradev_oracle @c:\AutoMated_DataLoad_Jobs\test\healthscope\scriptcopystagingprodhealthscope.sql > c:\AutoMated_DataLoad_Jobs\TEST\healthscope\LOG\scriptcopystagingprodhealthscope_%_my_datetime%.log
-#C:\Automated_DataLoad_Jobs\Test\healthscope\biomatching.bat > c:\AutoMated_DataLoad_Jobs\TEST\healthscope\LOG\biomatching_%_my_datetime%.log
+
+
+ECHO whenever sqlerror exit | sqlplus /@hra_oracle @c:\AutoMated_DataLoad_Jobs\test\healthscope\scriptcopystagingprodhealthscope.sql  > c:\AutoMated_DataLoad_Jobs\TEST\healthscope\LOG\healthscope.txt
+IF %ERRORLEVEL% NEQ 0 exit
+type c:\AutoMated_DataLoad_Jobs\TEST\healthscope\LOG\healthscope.txt > c:\AutoMated_DataLoad_Jobs\TEST\healthscope\LOG\scriptcopystagingprodhealthscope_%_my_datetime%.log
+C:\Automated_DataLoad_Jobs\Test\healthscope\ssn_dup.bat > c:\AutoMated_DataLoad_Jobs\TEST\healthscope\LOG\ssndup_%_my_datetime%.log
 C:\Automated_DataLoad_Jobs\Test\healthscope\healthscopedir.bat > c:\AutoMated_DataLoad_Jobs\TEST\healthscope\LOG\dir_%_my_datetime%.out
 
 
-
-
+WHENEVER sqlerror exit sql.sqlcode
 
 
 

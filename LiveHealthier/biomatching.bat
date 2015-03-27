897628b8c277cb082ce9347c48d8aa8e678b7e33
Julie Keros <julieker@umich.edu>
2015-03-27 16:51:46 -0400

    add links

--- LiveHealthier/biomatching.bat
+++ LiveHealthier/biomatching.bat
@@ -8,7 +8,7 @@ set _my_datetime=%_my_datetime:.=_%
 
 
 sqlplus /@hra_oracle @c:\AutoMated_DataLoad_Jobs\test\livehealthier\biomatching.sql > c:\AutoMated_DataLoad_Jobs\TEST\livehealthier\LOG\biomatching_%_my_datetime%.log
-
+sqlplus /@hraj_oracle @c:\AutoMated_DataLoad_Jobs\test\livehealthier\biomatching-report.sql > c:\AutoMated_DataLoad_Jobs\TEST\livehealthier\LOG\biomatching-report_%_my_datetime%.log
 
 
 

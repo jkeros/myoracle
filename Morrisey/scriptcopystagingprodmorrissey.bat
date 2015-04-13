3061b9afddcb0121831eed8e44cd1f4589760c0a
Julie Keros <julieker@umich.edu>
2015-04-13 17:36:11 -0400

    for notification

--- Morrisey/scriptcopystagingprodmorrissey.bat
+++ Morrisey/scriptcopystagingprodmorrissey.bat
@@ -7,12 +7,15 @@ set _my_datetime=%_my_datetime:/=_%
 set _my_datetime=%_my_datetime:.=_%
 
 
-sqlplus /@hra_oracle @c:\AutoMated_DataLoad_Jobs\test\morrissey\scriptcopystagingprodmorrissey.sql > c:\AutoMated_DataLoad_Jobs\TEST\morrissey\LOG\scriptcopystagingprodmorrissey_%_my_datetime%.log
-C:\Automated_DataLoad_Jobs\Test\morrissey\moriseydir.bat > c:\AutoMated_DataLoad_Jobs\TEST\morrissey\LOG\dir_%_my_datetime%.out
 
 
+ECHO whenever sqlerror exit | sqlplus /@hraj_oracle @c:\AutoMated_DataLoad_Jobs\test\morrissey\scriptcopystagingprodmorrissey.sql  > c:\AutoMated_DataLoad_Jobs\TEST\morrissey\LOG\morrissey.txt
+IF %ERRORLEVEL% NEQ 0 exit
+type c:\AutoMated_DataLoad_Jobs\TEST\morrissey\LOG\morrissey.txt > c:\AutoMated_DataLoad_Jobs\TEST\morrissey\LOG\scriptcopystagingprodmorrissey_%_my_datetime%.log
+C:\Automated_DataLoad_Jobs\Test\morrissey\morrisseydir.bat > c:\AutoMated_DataLoad_Jobs\TEST\morrissey\LOG\dir_%_my_datetime%.out
 
 
+WHENEVER sqlerror exit sql.sqlcode
 
 
 

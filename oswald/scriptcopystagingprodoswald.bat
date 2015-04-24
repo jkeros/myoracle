0fbdcfe4c35965b478799613c338f08ec4c3a9d2
Julie Keros <julieker@umich.edu>
2015-04-24 16:18:34 -0400

    add in automatic notification

--- oswald/scriptcopystagingprodoswald.bat
+++ oswald/scriptcopystagingprodoswald.bat
@@ -7,13 +7,15 @@ set _my_datetime=%_my_datetime:/=_%
 set _my_datetime=%_my_datetime:.=_%
 
 
-sqlplus /@hra_oracle @c:\AutoMated_DataLoad_Jobs\test\oswald\scriptcopystagingprodoswald.sql > c:\AutoMated_DataLoad_Jobs\TEST\oswald\LOG\scriptcopystagingprodoswald_%_my_datetime%.log
+
+
+ECHO whenever sqlerror exit | sqlplus /@hraj_oracle @c:\AutoMated_DataLoad_Jobs\test\oswald\scriptcopystagingprodoswald.sql  > c:\AutoMated_DataLoad_Jobs\TEST\oswald\LOG\oswald.txt
+IF %ERRORLEVEL% NEQ 0 exit
+type c:\AutoMated_DataLoad_Jobs\TEST\oswald\LOG\oswald.txt > c:\AutoMated_DataLoad_Jobs\TEST\oswald\LOG\scriptcopystagingprodoswald_%_my_datetime%.log
 C:\Automated_DataLoad_Jobs\Test\oswald\oswalddir.bat > c:\AutoMated_DataLoad_Jobs\TEST\oswald\LOG\dir_%_my_datetime%.out
 
 
-
-
-
+WHENEVER sqlerror exit sql.sqlcode
 
 
 

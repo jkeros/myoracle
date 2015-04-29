8abb2b4ac6daccd874c282c2525b872a61e70242
Julie Keros <julieker@umich.edu>
2015-04-29 17:07:26 -0400

    add notification

--- Wright_Check/scriptcopystagingprodwrightcheck.bat
+++ Wright_Check/scriptcopystagingprodwrightcheck.bat
@@ -7,12 +7,15 @@ set _my_datetime=%_my_datetime:/=_%
 set _my_datetime=%_my_datetime:.=_%
 
 
-sqlplus /@hra_oracle @c:\AutoMated_DataLoad_Jobs\test\wright_check\scriptcopystagingprodwrightcheck.sql > c:\AutoMated_DataLoad_Jobs\TEST\wright_check\LOG\scriptcopystagingprodwrightcheck_%_my_datetime%.log
+
+
+ECHO whenever sqlerror exit | sqlplus /@hraj_oracle @c:\AutoMated_DataLoad_Jobs\test\wright_check\scriptcopystagingprodwrightcheck.sql  > c:\AutoMated_DataLoad_Jobs\TEST\wright_check\LOG\wright_check.txt
+IF %ERRORLEVEL% NEQ 0 exit
+type c:\AutoMated_DataLoad_Jobs\TEST\wright_check\LOG\wright_check.txt > c:\AutoMated_DataLoad_Jobs\TEST\wright_check\LOG\scriptcopystagingprodwright_check_%_my_datetime%.log
 C:\Automated_DataLoad_Jobs\Test\wright_check\wrightcheckdir.bat > c:\AutoMated_DataLoad_Jobs\TEST\wright_check\LOG\dir_%_my_datetime%.out
 
 
-
-
+WHENEVER sqlerror exit sql.sqlcode
 
 
 

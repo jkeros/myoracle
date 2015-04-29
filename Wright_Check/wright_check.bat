2489cbdf75c10105d3808f83cfbd86a205920f44
Julie Keros <julieker@umich.edu>
2015-04-29 17:07:46 -0400

    add notification

--- Wright_Check/wright_check.bat
+++ Wright_Check/wright_check.bat
@@ -8,5 +8,4 @@ set _my_datetime=%_my_datetime:.=_%
 
 
 c:\AutoMated_DataLoad_Jobs\test\wright_check\wright_check.vbs  > c:\AutoMated_DataLoad_Jobs\TEST\wright_check\LOG\wright_check_%_my_datetime%.log
-c:\AutoMated_DataLoad_Jobs\test\wright_check\scriptcopystagingprodwrightcheck.bat  > c:\AutoMated_DataLoad_Jobs\TEST\wright_check\LOG\scriptcopystagingprodwright_check_%_my_datetime%.log
 c:\AutoMated_DataLoad_Jobs\test\wright_check\wrightcheckdir.bat > c:\AutoMated_DataLoad_Jobs\TEST\wright_check\LOG\dir.log

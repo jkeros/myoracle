76d12d402f16d186aeb768cd7d983f87dba2fbf9
Julie Keros <julieker@umich.edu>
2015-07-27 17:12:52 -0400

    alleigance error handling

--- Allegiance/runit.bat
+++ Allegiance/runit.bat
@@ -8,5 +8,5 @@ set _my_datetime=%_my_datetime:.=_%
 
 
 C:\Automated_DataLoad_Jobs\Test\Allegiance\openexcel.vbs
-C:\Automated_DataLoad_Jobs\Test\Allegiance\openexcellbio.vbs
-sqlplus /@hraj_oracle @C:\Automated_DataLoad_Jobs\Test\Allegiance\runit.txt > C:\Automated_DataLoad_Jobs\Test\Allegiance\log\insert_allegianceelig%_my_datetime%.log
+C:\Automated_DataLoad_Jobs\Test\Allegiance\openexcelbio.vbs
+
